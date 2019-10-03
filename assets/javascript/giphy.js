c65dc3c8b3aab3edbc74d4b179afcaea0e14732e
Wilder8 <jacob.wilder826@gmail.com>
2019-10-03 15:43:01 -0400

    added API key to JS file

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
 var topics = ["The Office", "Parks and Recreation", "American Horror Story", "Shameless"];
-
+var apiKey = "ilH3dpq8Ub4A64fnXqYToNczQ6CpfUCI"
+var urlQuery = ""
 
 
 
